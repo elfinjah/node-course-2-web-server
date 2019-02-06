6ca6b83f20c0e6846c6b55ef1bc127151b7d4364
Elfin Jonathan <Elfin_J0218@mitrais.com>
2019-02-06 11:25:32 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const _express = require('express');
+const _hbs = require('hbs');
+
+var app = _express(); 
+ 
+_hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+app.use(_express.static(__dirname + '/public'));
+
+_hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!!</h1>');
+    res.render('home.hbs',{
+        header: 'Welcome Elfin...',
+        hobby:[
+            'arduino',
+            'RC'
+        ]
+        //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        header: 'About Page'
+        //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "unable the request"
+    });
+});
+
+app.listen(3000);
